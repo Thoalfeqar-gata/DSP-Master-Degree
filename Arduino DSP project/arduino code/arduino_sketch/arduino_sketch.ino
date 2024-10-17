e296623b7eeef5ac0cc483455c149625f82191e1
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2024-10-17 13:56:08 +0300

    Finished the filter realization of the potentiometer and added an optional first order butterworth filter for the mpu6050 for faster response time

--- Arduino DSP project/arduino code/arduino_sketch/arduino_sketch.ino
+++ Arduino DSP project/arduino code/arduino_sketch/arduino_sketch.ino
@@ -15,6 +15,7 @@
 
 
 bool filter = true; //determines if the signals should be filtered or not
+int mpu_6050_filter_order = 1;
 int T = 1; //Time between two samples. In miliseconds!
 float angleX, angleY;
 float offsetX = 0, offsetY = 0;
@@ -23,7 +24,7 @@ float voltage = 0;
 MPU6050 mpu(Wire, 1, 0); //Construct the object without a filter.
 
 
-void turn_on_seven_segment(float angleX, float angleY, int neighborhood_tolerance = 10)
+void turn_on_seven_segment(float angleX, float angleY, float neighborhood_tolerance = 7.5)
 {
   //The following two lines of code are done because the orientation of the sensor is reversed.
   //We want the north and east to be indicated by positive angle values, while the 
@@ -96,23 +97,39 @@ void turn_on_seven_segment(float angleX, float angleY, int neighborhood_toleranc
   }
 }
 
-float y_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (x_angle readings)
-float x_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (X_angle readings)
 
-float read_mpu_6050_x(bool filter)
+float y_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (x_angle filtered readings), (will be used for either the first order filter by ignoring the last element of the array, or the second order filter)
+float x_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (X_angle readings), (will be used for either the first order filter by ignoring the last element of the array, or the second order filter)
+
+float read_mpu_6050_x(bool filter, int filter_order)
 {
   if(filter)
   {
-    x_angle_x[0] = mpu.getAngleX();
-    y_angle_x[0] = 1.95557824*y_angle_x[1] - 0.95654368*y_angle_x[2] + 0.00024136 * x_angle_x[0] + 0.00048272*x_angle_x[1] + 0.00024136*x_angle_x[2];
-    
-    //delay the samples
-    y_angle_x[2] = y_angle_x[1];
-    y_angle_x[1] = y_angle_x[0];
-    x_angle_x[2] = x_angle_x[1];
-    x_angle_x[1] = x_angle_x[0];
+    if(filter_order == 1)
+    {
+      x_angle_x[0] = mpu.getAngleX();
+      y_angle_x[0] = 0.96906742*y_angle_x[1] + 0.01546629*x_angle_x[0] + 0.01546629*x_angle_x[1];
 
-    return y_angle_x[0];
+      //delay the samples
+      y_angle_x[1] = y_angle_x[0];
+      x_angle_x[1] = x_angle_x[0];
+
+      return y_angle_x[0];
+    }
+    else if(filter_order == 2)
+    {
+      x_angle_x[0] = mpu.getAngleX();
+      y_angle_x[0] = 1.95557824*y_angle_x[1] - 0.95654368*y_angle_x[2] + 0.00024136 * x_angle_x[0] + 0.00048272*x_angle_x[1] + 0.00024136*x_angle_x[2];
+      
+      //delay the samples
+      y_angle_x[2] = y_angle_x[1];
+      y_angle_x[1] = y_angle_x[0];
+      x_angle_x[2] = x_angle_x[1];
+      x_angle_x[1] = x_angle_x[0];
+
+      return y_angle_x[0];
+    }
+    
   }
   else
   {
@@ -121,27 +138,64 @@ float read_mpu_6050_x(bool filter)
 }
 
 
-float y_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (y_angle_readings)
-float x_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (y_angle readings)
+float y_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (y_angle filtered readings), (will be used for either the first order filter by ignoring the last element of the array, or the second order filter)
+float x_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (y_angle readings), (will be used for either the first order filter by ignoring the last element of the array, or the second order filter)
 
-float read_mpu_6050_y(bool filter)
+float read_mpu_6050_y(bool filter, int filter_order)
 {
   if(filter)
   {
-    x_angle_y[0] = mpu.getAngleY();
-    y_angle_y[0] = 1.95557824*y_angle_y[1] - 0.95654368*y_angle_y[2] + 0.00024136 * x_angle_y[0] + 0.00048272*x_angle_y[1] + 0.00024136*x_angle_y[2];
-    
-    //delay the samples
-    y_angle_y[2] = y_angle_y[1];
-    y_angle_y[1] = y_angle_y[0];
-    x_angle_y[2] = x_angle_y[1];
-    x_angle_y[1] = x_angle_y[0];
+    if(filter_order == 1)
+    {
+      x_angle_y[0] = mpu.getAngleY();
+      y_angle_y[0] = 0.96906742*y_angle_y[1] + 0.01546629*x_angle_y[0] + 0.01546629*x_angle_y[1];
 
-    return y_angle_y[0];
+      //delay samples
+      y_angle_y[1] = y_angle_y[0];
+      x_angle_y[1] = x_angle_y[0];
+
+      return y_angle_y[0];
+    }
+    else if(filter_order == 2)
+    {
+      x_angle_y[0] = mpu.getAngleY();
+      y_angle_y[0] = 1.95557824*y_angle_y[1] - 0.95654368*y_angle_y[2] + 0.00024136 * x_angle_y[0] + 0.00048272*x_angle_y[1] + 0.00024136*x_angle_y[2];
+      
+      //delay the samples
+      y_angle_y[2] = y_angle_y[1];
+      y_angle_y[1] = y_angle_y[0];
+      x_angle_y[2] = x_angle_y[1];
+      x_angle_y[1] = x_angle_y[0];
+
+      return y_angle_y[0];
+    }
+   
   }
   else
   {
     return mpu.getAngleY();
+  }
+}
+
+float y_voltage[2] = {0.0, 0.0}; //an empty array for storing the output of the potentiometer filter
+float x_voltage[2] = {0.0, 0.0}; //an emptoy array for storing the input of the potentiometer (raw unfiltered data)
+
+float read_voltage_level(bool filter)
+{
+  if(filter)
+  {
+    x_voltage[0] = analogRead(A2) * 255.0 / 1024.0; //read and normalize the voltage to be between 0 and 255.
+    y_voltage[0] = 0.98751193*y_voltage[1] + 0.00624404*x_voltage[0] + 0.00624404*x_voltage[1];
+
+    //delay the samples
+    y_voltage[1] = y_voltage[0];
+    x_voltage[1] = x_voltage[0];
+
+    return y_voltage[0];
+  }
+  else
+  {
+    return analogRead(A2) * 255.0 / 1024.0; //return the unfiltered signal after normalizing it to be between 0 and 255.
   }
 }
 
@@ -156,8 +210,8 @@ void setup() {
   int samples_to_get = 10000;
   for(int i = 0; i < samples_to_get; i++)
   {
-    offsetX += read_mpu_6050_x(filter = filter);
-    offsetY += read_mpu_6050_y(filter = filter);
+    offsetX += read_mpu_6050_x(filter, mpu_6050_filter_order);
+    offsetY += read_mpu_6050_y(filter, mpu_6050_filter_order);
     delay(T);
   }
 
@@ -180,9 +234,9 @@ void setup() {
 
 void loop() {
   mpu.update();
-  angleX = read_mpu_6050_x(filter = filter) - offsetX;
-  angleY = read_mpu_6050_y(filter = filter) - offsetY;
-  voltage = (analogRead(A2) * 255.0 / 1024.0); //read and normalize the voltage to be between 0 and 255
+  angleX = read_mpu_6050_x(filter, mpu_6050_filter_order) - offsetX;
+  angleY = read_mpu_6050_y(filter, mpu_6050_filter_order) - offsetY;
+  voltage = read_voltage_level(filter = filter);
   analogWrite(brightness_pin, int(voltage));
   turn_on_seven_segment(angleX, angleY);
 
