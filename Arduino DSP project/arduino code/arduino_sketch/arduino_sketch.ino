6071c45e5335bd9574ecb36d82ac58c8f3927fac
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2024-10-17 12:41:36 +0300

    Added the project's presentation and implemented the MPU6050 filter

--- Arduino DSP project/arduino code/arduino_sketch/arduino_sketch.ino
+++ Arduino DSP project/arduino code/arduino_sketch/arduino_sketch.ino
@@ -14,13 +14,13 @@
 #define center_segment 10
 
 
-bool filter = false; //determines if the signals should be filtered or not
-int T = 1; //Time between two samples
+bool filter = true; //determines if the signals should be filtered or not
+int T = 1; //Time between two samples. In miliseconds!
 float angleX, angleY;
 float offsetX = 0, offsetY = 0;
 float voltage = 0;
 
-MPU6050 mpu(Wire, 1, 0);
+MPU6050 mpu(Wire, 1, 0); //Construct the object without a filter.
 
 
 void turn_on_seven_segment(float angleX, float angleY, int neighborhood_tolerance = 10)
@@ -96,6 +96,55 @@ void turn_on_seven_segment(float angleX, float angleY, int neighborhood_toleranc
   }
 }
 
+float y_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (x_angle readings)
+float x_angle_x[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (X_angle readings)
+
+float read_mpu_6050_x(bool filter)
+{
+  if(filter)
+  {
+    x_angle_x[0] = mpu.getAngleX();
+    y_angle_x[0] = 1.95557824*y_angle_x[1] - 0.95654368*y_angle_x[2] + 0.00024136 * x_angle_x[0] + 0.00048272*x_angle_x[1] + 0.00024136*x_angle_x[2];
+    
+    //delay the samples
+    y_angle_x[2] = y_angle_x[1];
+    y_angle_x[1] = y_angle_x[0];
+    x_angle_x[2] = x_angle_x[1];
+    x_angle_x[1] = x_angle_x[0];
+
+    return y_angle_x[0];
+  }
+  else
+  {
+    return mpu.getAngleX();
+  }
+}
+
+
+float y_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the output of the filter for MPU6050 (y_angle_readings)
+float x_angle_y[3] = {0.0, 0.0, 0.0}; //an empty array for storing the current and previous two values of the readings from the MPU6050 sensor (y_angle readings)
+
+float read_mpu_6050_y(bool filter)
+{
+  if(filter)
+  {
+    x_angle_y[0] = mpu.getAngleY();
+    y_angle_y[0] = 1.95557824*y_angle_y[1] - 0.95654368*y_angle_y[2] + 0.00024136 * x_angle_y[0] + 0.00048272*x_angle_y[1] + 0.00024136*x_angle_y[2];
+    
+    //delay the samples
+    y_angle_y[2] = y_angle_y[1];
+    y_angle_y[1] = y_angle_y[0];
+    x_angle_y[2] = x_angle_y[1];
+    x_angle_y[1] = x_angle_y[0];
+
+    return y_angle_y[0];
+  }
+  else
+  {
+    return mpu.getAngleY();
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -107,8 +156,8 @@ void setup() {
   int samples_to_get = 10000;
   for(int i = 0; i < samples_to_get; i++)
   {
-    offsetX += mpu.getAngleX();
-    offsetY += mpu.getAngleY();
+    offsetX += read_mpu_6050_x(filter = filter);
+    offsetY += read_mpu_6050_y(filter = filter);
     delay(T);
   }
 
@@ -131,8 +180,8 @@ void setup() {
 
 void loop() {
   mpu.update();
-  angleX = mpu.getAngleX() - offsetX;
-  angleY = mpu.getAngleY() - offsetY;
+  angleX = read_mpu_6050_x(filter = filter) - offsetX;
+  angleY = read_mpu_6050_y(filter = filter) - offsetY;
   voltage = (analogRead(A2) * 255.0 / 1024.0); //read and normalize the voltage to be between 0 and 255
   analogWrite(brightness_pin, int(voltage));
   turn_on_seven_segment(angleX, angleY);
